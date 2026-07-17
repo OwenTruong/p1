e7923ac7dafe9fc88d441f636a76503b5da656b1
Daniel Sarzynski <dsarzynski13116@gmail.com>
2026-07-17 18:09:31 -0400

    implement /api/auth/token route and change "get_auth_status" middleware to be a function within web.py

--- backend/src/data/authorization.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-class Authorization(BaseModel):
-  is_authenticated: bool = Field(default=False, examples=[True, False])
-  username: str | None = Field(default=None, examples=[None, "test_dev"])
-  error_message: str | None = Field(default=None, examples=[None, 'Session has expired. Please re-authenticate'])
--- backend/src/routers/web.py
+++ backend/src/routers/web.py
@@ -2,9 +2,12 @@ import logging
 from pathlib import Path
 from urllib.parse import urlencode
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel, Field
+
+from src.auth import decode_and_verify_token
 
 ROUTERS_DIR = Path(__file__).resolve().parent
 SRC_DIR = ROUTERS_DIR.parent
@@ -14,43 +17,57 @@ router = APIRouter()
 
 templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
 
+class Authorization(BaseModel):
+  is_authenticated: bool = Field(default=False, examples=[True, False])
+  username: str | None = Field(default=None, examples=[None, "test_dev"])
+  error_message: str | None = Field(default=None, examples=[None, 'Session has expired. Please re-authenticate'])
+
+def get_auth_status(request: Request) -> Authorization:
+  access_token = request.cookies.get("access_token", None)
+  if not access_token:
+    return Authorization(is_authenticated=False, username=None, error_message="No access token provided.")
+
+  try:
+    token_string = access_token.replace("Bearer ", "")
+    token_payload = decode_and_verify_token(token_string)
+    return Authorization(is_authenticated=True, username=token_payload.get("sub"), error_message=None)
+  except HTTPException as exc:
+    return Authorization(is_authenticated=False, username=None, error_message=exc.detail)
+  except Exception:
+    return Authorization(is_authenticated=False, username=None, error_message="Internal Server Error")
+
+
 @router.get("/", status_code=200, response_class=HTMLResponse)
 async def serve_home(request: Request):
   return templates.TemplateResponse(
     name="index.html",
-    context={
-      "request": request
-    }
+    context={"request": request}
   )
 
 @router.get("/login", status_code=200, response_class=HTMLResponse)
-async def serve_login(request: Request):
-  if request.state.authorization.is_authenticated:
+async def serve_login(request: Request, authorization: Authorization = Depends(get_auth_status)):
+  if authorization.is_authenticated:
     return RedirectResponse('/protected')
   return templates.TemplateResponse(
     name="login.html",
-    context={
-      "request": request
-    }
+    context={"request": request}
   )
 
 @router.get("/register", status_code=200, response_class=HTMLResponse)
-async def serve_register(request: Request):
-  if request.state.authorization.is_authenticated:
+async def serve_register(request: Request, authorization: Authorization = Depends(get_auth_status)):
+  if authorization.is_authenticated:
     return RedirectResponse('/protected')
   return templates.TemplateResponse(
     name="register.html",
-    context={
-      "request": request
-    }
+    context={"request": request}
   )
 
 @router.get("/protected", status_code=200, response_class=HTMLResponse)
-async def serve_protected(request: Request):
-  if not request.state.authorization.is_authenticated:
+async def serve_protected(request: Request, authorization: Authorization = Depends(get_auth_status)):
+  if not authorization.is_authenticated:
     query = {
       "error_name": "Status 403",
-      "error_message": request.state.authorization.error_message or ''
+      "error_message": authorization.error_message or ''
     }
     return RedirectResponse(url=f"/error?{urlencode(query)}")
   logging.info("Now serving protected page")
@@ -59,7 +76,7 @@ async def serve_protected(request: Request):
     context={
       "request": request,
       "user": {
-        "username": request.state.authorization.username
+        "username": authorization.username
       }
     }
   )
